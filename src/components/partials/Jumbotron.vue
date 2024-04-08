fe8df28b54590131614a1e7aac5b9479c0c2f223
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-04-08 17:45:35 +0200

    [add] jumbotron

--- src/components/partials/Jumbotron.vue
+++ src/components/partials/Jumbotron.vue
@@ -17,10 +17,10 @@ export default {
 
     <div class="my-container d-flex">
       <div class="testi">
-      <h6>New Challenges, New Skills</h6>
-      <h2>BUILD TOUR OWN LIFE COACHING BUSINESS</h2>
-      <p>Whole-Life Business Coaching for committed entrepreneurs</p>
-      <button class="btn btn-success">Get started today</button>
+      <h5>New Challenges, New Skills</h5>
+      <h2 class="py-2">BUILD TOUR OWN LIFE COACHING BUSINESS</h2>
+      <p class="py-2">Whole-Life Business Coaching for committed entrepreneurs</p>
+      <button class="btn btn-success"><strong>Get started today</strong></button>
 
     </div>
 
@@ -41,29 +41,37 @@ export default {
 @use '../../assets/scss/partials/general' as *;
 @use '../../assets/scss/partials/variables' as *;
 
-.my-container {
-  max-width: 1000px;
-  margin: 0 auto;
-}
+
 .jumbotron {
   background-color: $bgc-ghiaccio;
   width: 100%;
+  
 
   .testi {
-    width: 30%;
-    h6 {
+    width: 40%;
+    align-self: center;
+    h5 {
       color: $color-verde;
     }
-    .btn {
+    p {
+      color: $color-grigio;
+    }
+    
+    .btn-success {
       background-color: $color-verde;
     }
   }
 
-  .immagine{
-  background-image: url('../../assets/img/home-business-hero-global-image.png');
-  background-size: contain;
-  width: 70%;
-}
+    .immagine{
+    background-image: url('../../assets/img/home-business-hero-global-image.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    // width: 75%;
+    img {
+      margin-top: 20px;
+    }
+       
+  }
 }
 
 
